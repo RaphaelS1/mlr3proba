30e88cb8d17102e104a0cca5f479d1dd3fbe6012
RaphaelS1 <raphaelsonabend@gmail.com>
2024-02-19 21:53:25 +0000

    more tests

--- paper_scoring.r
+++ paper_scoring.r
@@ -1,4 +1,4 @@
-t = tsk("rats")
+t = tgen("simsurv")$generate(300)
 s = partition(t)
 p = lrn("surv.coxph")$train(t, s$train)$predict(t, s$test)
 m_proper = msr("surv.graf", proper = TRUE)
@@ -6,8 +6,15 @@ m_improper = msr("surv.graf", proper = FALSE)
 p$score(m_proper)
 p$score(m_improper)
 
-mean(abs(m_proper$scores - m_improper$scores))
+censored = t$truth(s$test)[, 2] == 0
+
 mean((m_proper$scores - m_improper$scores)^2)
+mean((m_proper$scores[censored] - m_improper$scores[censored])^2)
+mean((m_proper$scores[!censored] - m_improper$scores[!censored])^2)
+
+
 
 plot(m_proper$scores, m_improper$scores)
 abline(a = 0, b = 1)
+
+all(m_proper$scores <= m_improper$scores)
